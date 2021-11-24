64405ccd37a5e0067c4941fd80903365474a67ec
Denis <78980031+DenisZackharov@users.noreply.github.com>
2021-11-24 17:19:03 +0300

    Add description (#33)

--- app/graphql/types/send_answer_and_get_next_question_type.rb
+++ app/graphql/types/send_answer_and_get_next_question_type.rb
@@ -1,7 +1,7 @@
 module Types
   class SendAnswerAndGetNextQuestionType < Types::BaseObject
     field :correct_answers_count, Integer, null: false
-    field :correct_answer_value, String, null: false
+    field :correct_answer_value, String, null: false, description: "Correct answer value for previous answer"
     field :question, Types::TeamMemberType, null: false
   end
 end
