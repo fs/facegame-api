160c01d811e1f3cc997ff41eb6f8856f17b8cc0f
deniszackharov <denis.zaharov@flatstack.com>
2021-11-25 18:10:17 +0300

    Add deprecated option for unused fields

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,7 +3,8 @@ module Types
     field :signin, mutation: Mutations::SignIn
     field :update_token, mutation: Mutations::UpdateToken
     field :signout, mutation: Mutations::SignOut
-    field :create_result, mutation: Mutations::CreateResult
+    field :create_result, mutation: Mutations::CreateResult,
+                          deprecation_reason: "Unused, now used sendAnswerAndGetNextQuestion mutation"
     field :start_game, mutation: Mutations::StartGame
     field :send_answer_and_get_next_question, mutation: Mutations::SendAnswerAndGetNextQuestion
     field :end_game, mutation: Mutations::EndGame
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,7 +1,8 @@
 module Types
   class QueryType < Types::BaseObject
     field :me, resolver: Resolvers::CurrentUser
-    field :questions, resolver: Resolvers::Questions
+    field :questions, resolver: Resolvers::Questions,
+                      deprecation_reason: "Now used mutation sendAnswerAndGetNextQuestion instead of createResult"
     field :results_board, resolver: Resolvers::ResultsBoard
     field :popularity_rating, resolver: Resolvers::PopularityRating
     field :images, resolver: Resolvers::Images
