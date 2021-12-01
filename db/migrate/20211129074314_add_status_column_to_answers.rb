4e86731868a061f9c28e8ab417b7e66763f888b8
Denis <78980031+DenisZackharov@users.noreply.github.com>
2021-12-01 10:46:11 +0300

    Refactoring migrations

--- db/migrate/20211129074314_add_status_column_to_answers.rb
+++ db/migrate/20211129074314_add_status_column_to_answers.rb
@@ -1,5 +1,5 @@
 class AddStatusColumnToAnswers < ActiveRecord::Migration[6.0]
   def change
-    add_column :answers, :status, :string, null: false  
+    add_column :answers, :status, :string, null: false, default: :pending
   end
 end
