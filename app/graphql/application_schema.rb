f636150e137d8e081b0714f4d62c5aaeba39ce87
deniszackharov <denis.zaharov@flatstack.com>
2021-12-06 10:06:14 +0300

    Fix rubocop

--- app/graphql/application_schema.rb
+++ app/graphql/application_schema.rb
@@ -2,7 +2,7 @@ require "graphql/batch"
 
 class ApplicationSchema < GraphQL::Schema
   disable_introspection_entry_points if Rails.env.production?
-  
+
   mutation(Types::MutationType)
   query(Types::QueryType)
 
