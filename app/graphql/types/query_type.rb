b5c333005bd854b9e79ec63d7ac95cbdab24a287
deniszackharov <denis.zaharov@flatstack.com>
2021-12-07 14:15:45 +0300

    Delete images

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,5 @@ module Types
     field :me, resolver: Resolvers::CurrentUser
     field :results_board, resolver: Resolvers::ResultsBoard
     field :popularity_rating, resolver: Resolvers::PopularityRating
-    field :images, resolver: Resolvers::Images
   end
 end
