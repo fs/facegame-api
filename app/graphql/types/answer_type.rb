bc122c78baf90b325cac8d61bc660e710b499cd4
deniszackharov <denis.zaharov@flatstack.com>
2021-12-07 12:18:02 +0300

    Delete unused files

--- app/graphql/types/answer_type.rb
+++
@@ -1,8 +0,0 @@
-module Types
-  class AnswerType < Types::BaseObject
-    field :id, ID, null: false
-    field :question, QuestionType, null: false
-    field :value, String, null: true
-    field :status, String, null: false
-  end
-end
--- app/graphql/types/popularity_rating_type.rb
+++ app/graphql/types/popularity_rating_type.rb
@@ -1,9 +1,5 @@
 module Types
   class PopularityRatingType < Types::BaseObject
-    field :answers_count, Integer, null: false,
-                                   deprecation_reason: "Now using statistic string"
-    field :correct_answers_count, Integer, null: false,
-                                           deprecation_reason: "Now using statistic "
     field :statistic, String, null: false
     field :avatar_url, String, null: true
   end
