b9af63db6831edb9dc3b41a2cbc597be5a6796f8
deniszackharov <denis.zaharov@flatstack.com>
2021-12-08 14:12:05 +0300

    Fix migration 2

--- db/migrate/20211208095206_add_multiple_index_for_answers.rb
+++ db/migrate/20211208095206_add_multiple_index_for_answers.rb
@@ -3,6 +3,5 @@ class AddMultipleIndexForAnswers < ActiveRecord::Migration[6.0]
   
   def change
     add_index :answers, [:question_id, :created_at], algorithm: :concurrently
-    safety_assured { remove_column :results, :time_duration, :integer }
   end
 end
